6d0462c20baed33ea95ea3840e73d227c3d21479
VadimQw  <v-dashin@mail.ru>
2020-04-19 17:51:42 +0300

    Added algorithm for reversing array.

---
+++ ReverseArray.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+var firstString: [Character]    = ["n", "e", "x", "t"]
+var secondString: [Character]   = ["h", "o", "u", "s", "e"]
+var therdString: [Character]    = ["s", "o", "m", "e", " ", "1", "2", "3"]
+
+// firstAnswer:     ["t", "x", "e", "n"]
+// secondAnswer:    ["e", "s", "u", "o", "h"]
+// therdAnswer:     ["3", "2", "1", " ", "e", "m", "o", "s"]
+
+// -
+
+func reverseArray<T>(_ array: inout [T]) {
+    
+    let lastIndex = array.count - 1
+    let bound = Int(lastIndex / 2)
+    
+    for index in 0...bound {
+        let reversedIndex = lastIndex - index
+        (array[index], array[reversedIndex]) = (array[reversedIndex], array[index])
+    }
+}
+
+// -
+
+reverseArray(&firstString)
+reverseArray(&secondString)
+reverseArray(&therdString)
+
+print(firstString)
+print(secondString)
+print(therdString)
